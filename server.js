1b8158058aad61c36a0aa5fb12f32b3b1f95956e
Aman Rajpoot <amanrajpoot5612@gmail.com>
2026-04-09 15:37:54 +0530

    Comment out app.listen and console log
    Comment out the server listening function and log statement.

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`AI Impact Reporting Generator running skjdfbhcnkvjbivu b9p8yh3 on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`AI Impact Reporting Generator running skjdfbhcnkvjbivu b9p8yh3 on http://localhost:${PORT}`);
+// });
